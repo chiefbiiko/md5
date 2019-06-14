2093fe2a3f853eff4bfd65865fb36d69eb956083
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-06-14 18:53:22 +0200

    baseline md5

--- mod.ts
+++ mod.ts
@@ -1,14 +1,91 @@
-/** 32-bit addition. Wrapping at 2**32. */
+import { decode } from "https://denopkg.com/chiefbiiko/std-encoding/mod.ts";
+
+export const BYTES: number = 16;
+
+function md5cycle(x: number[], k: number[]): void {
+  let a: number = x[0];
+  let b: number = x[1];
+  let c: number = x[2];
+  let d: number = x[3];
+
+  a = ff(a, b, c, d, k[0], 7, -680876936);
+  d = ff(d, a, b, c, k[1], 12, -389564586);
+  c = ff(c, d, a, b, k[2], 17, 606105819);
+  b = ff(b, c, d, a, k[3], 22, -1044525330);
+  a = ff(a, b, c, d, k[4], 7, -176418897);
+  d = ff(d, a, b, c, k[5], 12, 1200080426);
+  c = ff(c, d, a, b, k[6], 17, -1473231341);
+  b = ff(b, c, d, a, k[7], 22, -45705983);
+  a = ff(a, b, c, d, k[8], 7, 1770035416);
+  d = ff(d, a, b, c, k[9], 12, -1958414417);
+  c = ff(c, d, a, b, k[10], 17, -42063);
+  b = ff(b, c, d, a, k[11], 22, -1990404162);
+  a = ff(a, b, c, d, k[12], 7, 1804603682);
+  d = ff(d, a, b, c, k[13], 12, -40341101);
+  c = ff(c, d, a, b, k[14], 17, -1502002290);
+  b = ff(b, c, d, a, k[15], 22, 1236535329);
+
+  a = gg(a, b, c, d, k[1], 5, -165796510);
+  d = gg(d, a, b, c, k[6], 9, -1069501632);
+  c = gg(c, d, a, b, k[11], 14, 643717713);
+  b = gg(b, c, d, a, k[0], 20, -373897302);
+  a = gg(a, b, c, d, k[5], 5, -701558691);
+  d = gg(d, a, b, c, k[10], 9, 38016083);
+  c = gg(c, d, a, b, k[15], 14, -660478335);
+  b = gg(b, c, d, a, k[4], 20, -405537848);
+  a = gg(a, b, c, d, k[9], 5, 568446438);
+  d = gg(d, a, b, c, k[14], 9, -1019803690);
+  c = gg(c, d, a, b, k[3], 14, -187363961);
+  b = gg(b, c, d, a, k[8], 20, 1163531501);
+  a = gg(a, b, c, d, k[13], 5, -1444681467);
+  d = gg(d, a, b, c, k[2], 9, -51403784);
+  c = gg(c, d, a, b, k[7], 14, 1735328473);
+  b = gg(b, c, d, a, k[12], 20, -1926607734);
+
+  a = hh(a, b, c, d, k[5], 4, -378558);
+  d = hh(d, a, b, c, k[8], 11, -2022574463);
+  c = hh(c, d, a, b, k[11], 16, 1839030562);
+  b = hh(b, c, d, a, k[14], 23, -35309556);
+  a = hh(a, b, c, d, k[1], 4, -1530992060);
+  d = hh(d, a, b, c, k[4], 11, 1272893353);
+  c = hh(c, d, a, b, k[7], 16, -155497632);
+  b = hh(b, c, d, a, k[10], 23, -1094730640);
+  a = hh(a, b, c, d, k[13], 4, 681279174);
+  d = hh(d, a, b, c, k[0], 11, -358537222);
+  c = hh(c, d, a, b, k[3], 16, -722521979);
+  b = hh(b, c, d, a, k[6], 23, 76029189);
+  a = hh(a, b, c, d, k[9], 4, -640364487);
+  d = hh(d, a, b, c, k[12], 11, -421815835);
+  c = hh(c, d, a, b, k[15], 16, 530742520);
+  b = hh(b, c, d, a, k[2], 23, -995338651);
+
+  a = ii(a, b, c, d, k[0], 6, -198630844);
+  d = ii(d, a, b, c, k[7], 10, 1126891415);
+  c = ii(c, d, a, b, k[14], 15, -1416354905);
+  b = ii(b, c, d, a, k[5], 21, -57434055);
+  a = ii(a, b, c, d, k[12], 6, 1700485571);
+  d = ii(d, a, b, c, k[3], 10, -1894986606);
+  c = ii(c, d, a, b, k[10], 15, -1051523);
+  b = ii(b, c, d, a, k[1], 21, -2054922799);
+  a = ii(a, b, c, d, k[8], 6, 1873313359);
+  d = ii(d, a, b, c, k[15], 10, -30611744);
+  c = ii(c, d, a, b, k[6], 15, -1560198380);
+  b = ii(b, c, d, a, k[13], 21, 1309151649);
+  a = ii(a, b, c, d, k[4], 6, -145523070);
+  d = ii(d, a, b, c, k[11], 10, -1120210379);
+  c = ii(c, d, a, b, k[2], 15, 718787259);
+  b = ii(b, c, d, a, k[9], 21, -343485551);
+
+  x[0] = add32(a, x[0]);
+  x[1] = add32(b, x[1]);
+  x[2] = add32(c, x[2]);
+  x[3] = add32(d, x[3]);
+}
+
 function add32(a: number, b: number): number {
   return (a + b) & 0xffffffff;
 }
 
-/* Bitwise rotate a 32-bit number to the left. */
-function rotl(v: number, n: number) {
-  return (v << n) | (v >>> (32 - n));
-}
-
-/** Base op. */
 function cmn(
   q: number,
   a: number,
@@ -17,10 +94,10 @@ function cmn(
   s: number,
   t: number
 ): number {
-  return add32(rotl(add32(add32(a, q), add32(x, t)), s), b);
+  a = add32(add32(a, q), add32(x, t));
+  return add32((a << s) | (a >>> (32 - s)), b);
 }
 
-/** ff transform. */
 function ff(
   a: number,
   b: number,
@@ -33,7 +110,6 @@ function ff(
   return cmn((b & c) | (~b & d), a, b, x, s, t);
 }
 
-/** gg transform. */
 function gg(
   a: number,
   b: number,
@@ -46,7 +122,6 @@ function gg(
   return cmn((b & d) | (c & ~d), a, b, x, s, t);
 }
 
-/** hh transform. */
 function hh(
   a: number,
   b: number,
@@ -59,7 +134,6 @@ function hh(
   return cmn(b ^ c ^ d, a, b, x, s, t);
 }
 
-/** ii transform. */
 function ii(
   a: number,
   b: number,
@@ -72,129 +146,76 @@ function ii(
   return cmn(c ^ (b | ~d), a, b, x, s, t);
 }
 
-/** Serializes 4 ints to 16 little endian bytes. */
-function numberToFourLittleEndianBytes(
-  v: number,
-  out: Uint8Array,
-  offset: number = 0
-): void {
-  const loopEnd: number = offset + 4;
-  for (let i: number = 0 + offset; i < loopEnd; ++i) {
-    out[i] = v & 0xff;
-    v >>= 8;
+function md5blk(s: string): number[] {
+  let md5blks: number[] = [];
+
+  for (let i: number = 0; i < 64; i += 4) {
+    md5blks[i >> 2] =
+      s.charCodeAt(i) +
+      (s.charCodeAt(i + 1) << 8) +
+      (s.charCodeAt(i + 2) << 16) +
+      (s.charCodeAt(i + 3) << 24);
   }
+
+  return md5blks;
 }
 
-/* Calculate the MD5 of an array of little-endian words, and a bit length. */
-export function md5(x: Uint8Array): Uint8Array {
-  // x = x.reverse();
-
-  const out: Uint8Array = new Uint8Array(16);
-
-  /* append padding */
-  const len: number = x.byteLength * 8;
-  x[len >> 5] |= 0x80 << len % 32;
-  x[(((len + 64) >>> 9) << 4) + 14] = len;
-
+function md51(s: string): number[] {
+  const n: number = s.length;
+  const state: number[] = [1732584193, -271733879, -1732584194, 271733878];
+  const tail: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   let i: number;
-  let olda: number;
-  let oldb: number;
-  let oldc: number;
-  let oldd: number;
-  let a: number = 1732584193;
-  let b: number = -271733879;
-  let c: number = -1732584194;
-  let d: number = 271733878;
 
-  for (i = 0; i < x.length; i += 16) {
-    olda = a;
-    oldb = b;
-    oldc = c;
-    oldd = d;
-
-    a = ff(a, b, c, d, x[i], 7, -680876936);
-    d = ff(d, a, b, c, x[i + 1], 12, -389564586);
-    c = ff(c, d, a, b, x[i + 2], 17, 606105819);
-    b = ff(b, c, d, a, x[i + 3], 22, -1044525330);
-    a = ff(a, b, c, d, x[i + 4], 7, -176418897);
-    d = ff(d, a, b, c, x[i + 5], 12, 1200080426);
-    c = ff(c, d, a, b, x[i + 6], 17, -1473231341);
-    b = ff(b, c, d, a, x[i + 7], 22, -45705983);
-    a = ff(a, b, c, d, x[i + 8], 7, 1770035416);
-    d = ff(d, a, b, c, x[i + 9], 12, -1958414417);
-    c = ff(c, d, a, b, x[i + 10], 17, -42063);
-    b = ff(b, c, d, a, x[i + 11], 22, -1990404162);
-    a = ff(a, b, c, d, x[i + 12], 7, 1804603682);
-    d = ff(d, a, b, c, x[i + 13], 12, -40341101);
-    c = ff(c, d, a, b, x[i + 14], 17, -1502002290);
-    b = ff(b, c, d, a, x[i + 15], 22, 1236535329);
-
-    a = gg(a, b, c, d, x[i + 1], 5, -165796510);
-    d = gg(d, a, b, c, x[i + 6], 9, -1069501632);
-    c = gg(c, d, a, b, x[i + 11], 14, 643717713);
-    b = gg(b, c, d, a, x[i], 20, -373897302);
-    a = gg(a, b, c, d, x[i + 5], 5, -701558691);
-    d = gg(d, a, b, c, x[i + 10], 9, 38016083);
-    c = gg(c, d, a, b, x[i + 15], 14, -660478335);
-    b = gg(b, c, d, a, x[i + 4], 20, -405537848);
-    a = gg(a, b, c, d, x[i + 9], 5, 568446438);
-    d = gg(d, a, b, c, x[i + 14], 9, -1019803690);
-    c = gg(c, d, a, b, x[i + 3], 14, -187363961);
-    b = gg(b, c, d, a, x[i + 8], 20, 1163531501);
-    a = gg(a, b, c, d, x[i + 13], 5, -1444681467);
-    d = gg(d, a, b, c, x[i + 2], 9, -51403784);
-    c = gg(c, d, a, b, x[i + 7], 14, 1735328473);
-    b = gg(b, c, d, a, x[i + 12], 20, -1926607734);
-
-    a = hh(a, b, c, d, x[i + 5], 4, -378558);
-    d = hh(d, a, b, c, x[i + 8], 11, -2022574463);
-    c = hh(c, d, a, b, x[i + 11], 16, 1839030562);
-    b = hh(b, c, d, a, x[i + 14], 23, -35309556);
-    a = hh(a, b, c, d, x[i + 1], 4, -1530992060);
-    d = hh(d, a, b, c, x[i + 4], 11, 1272893353);
-    c = hh(c, d, a, b, x[i + 7], 16, -155497632);
-    b = hh(b, c, d, a, x[i + 10], 23, -1094730640);
-    a = hh(a, b, c, d, x[i + 13], 4, 681279174);
-    d = hh(d, a, b, c, x[i], 11, -358537222);
-    c = hh(c, d, a, b, x[i + 3], 16, -722521979);
-    b = hh(b, c, d, a, x[i + 6], 23, 76029189);
-    a = hh(a, b, c, d, x[i + 9], 4, -640364487);
-    d = hh(d, a, b, c, x[i + 12], 11, -421815835);
-    c = hh(c, d, a, b, x[i + 15], 16, 530742520);
-    b = hh(b, c, d, a, x[i + 2], 23, -995338651);
-
-    a = ii(a, b, c, d, x[i], 6, -198630844);
-    d = ii(d, a, b, c, x[i + 7], 10, 1126891415);
-    c = ii(c, d, a, b, x[i + 14], 15, -1416354905);
-    b = ii(b, c, d, a, x[i + 5], 21, -57434055);
-    a = ii(a, b, c, d, x[i + 12], 6, 1700485571);
-    d = ii(d, a, b, c, x[i + 3], 10, -1894986606);
-    c = ii(c, d, a, b, x[i + 10], 15, -1051523);
-    b = ii(b, c, d, a, x[i + 1], 21, -2054922799);
-    a = ii(a, b, c, d, x[i + 8], 6, 1873313359);
-    d = ii(d, a, b, c, x[i + 15], 10, -30611744);
-    c = ii(c, d, a, b, x[i + 6], 15, -1560198380);
-    b = ii(b, c, d, a, x[i + 13], 21, 1309151649);
-    a = ii(a, b, c, d, x[i + 4], 6, -145523070);
-    d = ii(d, a, b, c, x[i + 11], 10, -1120210379);
-    c = ii(c, d, a, b, x[i + 2], 15, 718787259);
-    b = ii(b, c, d, a, x[i + 9], 21, -343485551);
-
-    a = add32(a, olda);
-    b = add32(b, oldb);
-    c = add32(c, oldc);
-    d = add32(d, oldd);
+  for (i = 64; i <= s.length; i += 64) {
+    md5cycle(state, md5blk(s.substring(i - 64, i)));
   }
-  // return [a, b, c, d]
-  numberToFourLittleEndianBytes(a, out, 0);
-  numberToFourLittleEndianBytes(b, out, 4);
-  numberToFourLittleEndianBytes(c, out, 8);
-  numberToFourLittleEndianBytes(d, out, 12);
-  // const view: DataView = new DataView(out.buffer);
-  // view.setInt32(0, a, true)
-  // view.setInt32(4, b, true)
-  // view.setInt32(8, c, true)
-  // view.setInt32(12, d, true)
 
-  return out;
+  s = s.substring(i - 64);
+
+  for (i = 0; i < s.length; i++) {
+    tail[i >> 2] |= s.charCodeAt(i) << (i % 4 << 3);
+  }
+
+  tail[i >> 2] |= 0x80 << (i % 4 << 3);
+
+  if (i > 55) {
+    md5cycle(state, tail);
+
+    for (i = 0; i < 16; i++) {
+      tail[i] = 0;
+    }
+  }
+
+  tail[14] = n * 8;
+
+  md5cycle(state, tail);
+
+  return state;
+}
+
+function writeLE(v: number, buf: Uint8Array, offset: number): void {
+  buf[offset] = v & 0xff;
+  buf[offset + 1] = (v >> 8) & 0xff;
+  buf[offset + 2] = (v >> 16) & 0xff;
+  buf[offset + 3] = (v >> 24) & 0xff;
+}
+
+export function md5(
+  msg: string | Uint8Array,
+  inputEncoding?: string,
+  outputEncoding?: string
+): string | Uint8Array {
+  if (msg instanceof Uint8Array) {
+    msg = decode(msg, inputEncoding) as string;
+  }
+
+  const state: number[] = md51(msg);
+  const buf: Uint8Array = new Uint8Array(BYTES);
+
+  writeLE(state[0], buf, 0);
+  writeLE(state[1], buf, 4);
+  writeLE(state[2], buf, 8);
+  writeLE(state[3], buf, 12);
+
+  return outputEncoding ? decode(buf, outputEncoding) : buf;
 }
